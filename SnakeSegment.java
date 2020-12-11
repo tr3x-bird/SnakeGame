6847b1a8fb8a05277f0e5876964b2959cbc51dcf
tr3x-bird <73552912+tr3x-bird@users.noreply.github.com>
2020-12-11 17:35:31 +0100

    Add files via upload
    Added SnakeSegment Class

---
+++ SnakeSegment.java
@@ -0,0 +1,24 @@
+
+public class SnakeSegment {
+	private int xCor;
+	private int yCor;
+	
+	public SnakeSegment(int x, int y) {
+		this.xCor = x;
+		this.yCor = y;
+	}
+	
+	public int getXCor() {
+		return xCor;
+	}
+	
+	public int getYCor() {
+		return yCor;
+	}
+	
+	public void setCor(int x, int y) {
+		this.xCor = x;
+		this.yCor = y;
+	}
+
+}
